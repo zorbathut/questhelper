725755a94c6199b192115aafbf38d5fa9d7abdb4
smariot <qhaddon@gmail.com>
2007-12-22 09:50:15 -0500

    Game no longer freezes while searching, and displays multiple search results via a menu.

--- custom.lua
+++ custom.lua
@@ -70,153 +70,258 @@ local function fuzzyCompare(a, b)
   return row[n+1]/math.max(n,m)
 end
 
--- Okay, addon related stuff here.
+local search_frame = CreateFrame("Button", nil, UIParent)
+search_frame.text = search_frame:CreateFontString()
+search_frame.text:SetFont("Fonts\\ARIALN.TTF", 15)
+search_frame.text:SetTextColor(0, 0, 0)
+search_frame.text:SetJustifyH("CENTER")
+search_frame.text:SetJustifyV("MIDDLE")
+search_frame.text:SetDrawLayer("OVERLAY")
+search_frame.text:SetAllPoints()
+search_frame.text:Show()
+search_frame.background = search_frame:CreateTexture()
+search_frame.background:SetTexture(1, 1, 1, 0.5)
+search_frame.background:SetDrawLayer("BACKGROUND")
+search_frame.background:SetAllPoints()
+search_frame.background:Show()
+search_frame:SetPoint("CENTER", UIParent, "CENTER")
+search_frame:Hide()
 
-local function locationRead(command)
-  -- string.find(command, "^%s*([^%d%.]+)([%d%.]+)%s+,?([%d%.]+)%s+$")
-  local _, _, region, x, y = string.find(command, "^%s*([^%d%.]-)%s*([%d%.]+)%s*[,;:]?%s*([%d%.]+)%s*$")
+search_frame.results = {}
+search_frame.spare_tables = {}
+
+function search_frame:SetText(text)
+  self.text:SetText(text)
+  self:SetWidth(self.text:GetWidth()+10)
+  self:SetHeight(self.text:GetHeight()+10)
+end
+
+function search_frame:OnUpdate()
+  if self.routine and coroutine.status(self.routine) ~= "dead" then
+    local no_error, display = coroutine.resume(self.routine, self, self.query)
+    if no_error then
+      self:SetText(display)
+    else
+      QuestHelper:TextOut("Searching co-routine just exploded: "..display)
+    end
+  else
+    self:ShowResults()
+    self.routine = nil
+    self:SetScript("OnUpdate", nil)
+    self:Hide()
+  end
+end
+
+function QuestHelper:ToggleUserObjective(cat, what)
+  local objective = self:GetObjective(cat, what)
   
-  if region and x and y then
-    x, y = tonumber(x), tonumber(y)
-    if x and y then
-      if region == "" then
-        return "loc", string.format("%d,%d,%.3f,%.3f", QuestHelper.c, QuestHelper.z, x/100, y/100), 1
-      else
-        local bc, bz, bn, score
+  if self.user_objectives[objective] then
+    self:TextOut("Removed: "..self.user_objectives[objective])
+    self:RemoveObjectiveWatch(objective, self.user_objectives[objective])
+    self.user_objectives[objective] = nil
+  elseif objective:Known() then
+    local name
+    if cat == "loc" then
+      local _, _, c, z, x, y = string.find(what, "^(%d+),(%d+),([%d%.]+),([%d%.]+)$")
+      name = "User Objective: "..self:HighlightText(select(z,GetMapZones(c)))..": "..self:HighlightText(x*100)..", "..self:HighlightText(y*100)
+    else
+      name = "User Objective: "..self:HighlightText(string.gsub(cat, "^(.)", string.upper))..": "..self:HighlightText(what)
+    end
+    
+    self.user_objectives[objective] = name
+    self:AddObjectiveWatch(objective, name)
+    
+    self:TextOut("Created: "..name)
+  else
+    self:TextOut("I don't know where you should go for that objective.")
+  end
+end
+
+function search_frame:CreateResultItem(r, menu)
+  local item
+  
+  if r.cat == "loc" then
+    local _, _, c, z, x, y = string.find(r.what, "^(%d+),(%d+),([%d%.]+),([%d%.]+)$")
+    item = QuestHelper:CreateMenuItem(menu, select(z,GetMapZones(c))..": "..(x*100)..", "..(y*100).." ["..QuestHelper:PercentString(1-r.w).."]")
+    item:AddTexture(QuestHelper:GetIconTexture(item, 6), true)
+  else
+    item = QuestHelper:CreateMenuItem(menu, r.what .. " ["..QuestHelper:PercentString(1-r.w).."]")
+    item:AddTexture(QuestHelper:GetIconTexture(item, (r.cat == "monster" and 1) or 2), true)
+  end
+  
+  item:SetFunction(QuestHelper.ToggleUserObjective, QuestHelper, r.cat, r.what)
+  
+  return item
+end
+
+function search_frame:ShowResults()
+  local menu = QuestHelper:CreateMenu()
+  QuestHelper:CreateMenuTitle(menu, "Search Results")
+  
+  for i, r in ipairs(self.results) do
+    self:CreateResultItem(r, menu)
+  end
+  
+  menu:ShowAtCursor()
+  self:ClearResults()
+end
+
+function search_frame:ClearResults()
+  while #self.results > 0 do
+    table.insert(self.spare_tables, table.remove(self.results))
+  end
+end
+
+function search_frame:AddResult(cat, what, w)
+  local r = self.results
+  local mn, mx = 1, #r+1
+  
+  while mn ~= mx do
+    local m = math.floor((mn+mx)*0.5)
+    
+    if r[m].w < w then
+      mn = m+1
+    else
+      mx = m
+    end
+  end
+  
+  if mn <= 20 then
+    if r[mn] and r[mn].cat == cat and r[mn].what == what then
+      -- Don't add the same item twice.
+      -- Might miss it if multiple items have the same score. Dont care.
+      return
+    end
+    
+    if #r >= 20 then
+      table.insert(self.spare_tables, table.remove(r, 20))
+    end
+    local obj = table.remove(self.spare_tables) or {}
+    obj.cat = cat
+    obj.what = what
+    obj.w = w
+    table.insert(r, mn, obj)
+  end
+end
+
+function search_frame:SearchRoutine(input)
+  input = string.upper(input)
+  local _, _, command, argument = string.find(input, "^%s*([^%s]-)%s+(.-)%s*$")
+  
+  local search_item, search_npc, search_loc = false, false, false
+  
+  if command and argument then
+    if command == "ITEM" then
+      search_item, input = true, argument
+    elseif command == "NPC" or command == "MONSTER" then
+      search_npc, input = true, argument
+    elseif command == "LOCATION" or command == "LOC" then
+      search_loc, input = true, argument
+    else
+      search_item, search_npc, search_loc = true, true, true
+    end
+  else
+    search_item, search_npc, search_loc = true, true, true
+  end
+  
+  local yield_countdown_max = math.max(1, math.floor(2000/string.len(input)+0.5))
+  local yield_countdown = yield_countdown_max
+  
+  if search_item then
+    local list = QuestHelper_Objectives["item"]
+    if list then for n in pairs(list) do
+      self:AddResult("item", n, fuzzyCompare(input, string.upper(n)))
+      yield_countdown = yield_countdown - 1
+      if yield_countdown == 0 then
+        yield_countdown = yield_countdown_max
+        coroutine.yield("Searching: Local Items")
+      end
+    end end
+    
+    list = QuestHelper_StaticData[QuestHelper.locale].objective
+    list = list and list.item
+    if list then for n in pairs(list) do
+      self:AddResult("item", n, fuzzyCompare(input, string.upper(n)))
+      yield_countdown = yield_countdown - 1
+      if yield_countdown == 0 then
+        yield_countdown = yield_countdown_max
+        coroutine.yield("Searching: Static Items")
+      end
+    end end
+  end
+  
+  if search_npc then
+    local list = QuestHelper_Objectives["monster"]
+    if list then for n in pairs(list) do
+      self:AddResult("monster", n, fuzzyCompare(input, string.upper(n)))
+      yield_countdown = yield_countdown - 1
+      if yield_countdown == 0 then
+        yield_countdown = yield_countdown_max
+        coroutine.yield("Searching: Local NPCs")
+      end
+    end end
+    
+    list = QuestHelper_StaticData[QuestHelper.locale].objective
+    list = list and list.monster
+    if list then for n in pairs(list) do
+      self:AddResult("monster", n, fuzzyCompare(input, string.upper(n)))
+      yield_countdown = yield_countdown - 1
+      if yield_countdown == 0 then
+        yield_countdown = yield_countdown_max
+        coroutine.yield("Searching: Static NPCs")
+      end
+    end end
+  end
+  
+  if search_loc then
+    local _, _, region, x, y = string.find(input, "^%s*([^%d%.]-)%s*([%d%.]+)%s*[,;:]?%s*([%d%.]+)%s*$")
+    
+    if region then
+      x, y = tonumber(x), tonumber(y)
+      if x and y then
+        x, y = x*0.01, y*0.01
         
-        for c=1,3 do
-          local z = 1
-          while true do
-            local zone_name = select(z,GetMapZones(c))
-            if zone_name then
-              s = fuzzyCompare(region, string.upper(zone_name))
-              if not score or s < score then
-                bc, bz, bn, score = c, z, zone_name, s
+        if region == "" then
+          self:AddResult("loc", string.format("%d,%d,%.3f,%.3f", QuestHelper.c, QuestHelper.z, x, y), 1)
+        else
+          for c=1,3 do
+            local z = 1
+            while true do
+              local zone_name = select(z,GetMapZones(c))
+              if zone_name then
+                self:AddResult("loc", string.format("%d,%d,%.3f,%.3f", c, z, x, y), fuzzyCompare(region, string.upper(zone_name)))
+                yield_countdown = yield_countdown - 1
+                if yield_countdown == 0 then
+                  yield_countdown = yield_countdown_max
+                  coroutine.yield("Searching: Zones")
+                end
+                z = z + 1
+              else
+                break
               end
-              z = z + 1
-            else
-              break
             end
           end
         end
-        
-        if bc and bz then
-          return "loc", string.format("%d,%d,%.3f,%.3f", bc, bz, x/100, y/100), score
-        end
       end
     end
   end
+  
+  return "Done!"
 end
 
-local function npcRead(command)
-  local name, score
-  local list = QuestHelper_Objectives["monster"]
-  if list then for n in pairs(list) do
-    local s = fuzzyCompare(command, string.upper(n))
-    if not score or s < score then
-      name, score = n, s
-    end
-  end end
-  
-  list = QuestHelper_StaticData[QuestHelper.locale].objective
-  list = list and list.monster
-  if list then for n in pairs(list) do
-    local s = fuzzyCompare(command, string.upper(n))
-    if not score or s < score then
-      name, score = n, s
-    end
-  end end
-  
-  if score then
-    return "monster", name, score
+function search_frame:PerformSearch(input)
+  if not self.routine then
+    self.query = input
+    self.routine = coroutine.create(self.SearchRoutine)
+    self:Show()
+    self:SetScript("OnUpdate", self.OnUpdate)
   end
 end
 
-local function itemRead(command)
-  local name, score
-  local list = QuestHelper_Objectives["item"]
-  if list then for n in pairs(list) do
-    local s = fuzzyCompare(command, string.upper(n))
-    if not score or s < score then
-      name, score = n, s
-    end
-  end end
-  
-  list = QuestHelper_StaticData[QuestHelper.locale].objective
-  list = list and list.item
-  if list then for n in pairs(list) do
-    local s = fuzzyCompare(command, string.upper(n))
-    if not score or s < score then
-      name, score = n, s
-    end
-  end end
-  
-  if score then
-    return "item", name, score
-  end
+function QuestHelper:PerformSearch(query)
+  search_frame:PerformSearch(query)
 end
 
-local function improve(category, what, score, new_category, new_what, new_score)
-  if new_score and (not score or new_score < score) then
-    return new_category, new_what, new_score
-  else
-    return category, what, score
-  end
-end
-
-local function findObjective(command_string)
-  command_string = string.upper(command_string)
-  local _, _, command, argument = string.find(command_string, "^%s*([^%s]-)%s+(.-)%s*$")
-  if command and argument then
-    if command == "ITEM" then
-      return itemRead(argument)
-    elseif command == "NPC" or command == "MONSTER" then
-      return npcRead(argument)
-    elseif command == "LOCATION" or command == "LOC" then
-      return locationRead(argument)
-    end
-  end
-  
-  local cat, what, s = improve(nil, nil, nil, locationRead(command_string))
-  cat, what, s = improve(cat, what, s, npcRead(command_string))
-  return improve(cat, what, s, itemRead(command_string))
-end
-
-local function slashHelper(command_string)
-  local cat, what = findObjective(command_string)
-  
-  if cat and what then
-    local objective = QuestHelper:GetObjective(cat, what)
-    
-    if QuestHelper.user_objectives[objective] then
-      QuestHelper:TextOut("Removed: "..QuestHelper.user_objectives[objective])
-      QuestHelper:RemoveObjectiveWatch(objective, QuestHelper.user_objectives[objective])
-      QuestHelper.user_objectives[objective] = nil
-      return
-    elseif objective:Known() then
-      local name
-      if cat == "loc" then
-        local _, _, c, z, x, y = string.find(what, "^(%d+),(%d+),([%d%.]+),([%d%.]+)$")
-        name = "User Objective: "..QuestHelper:HighlightText(select(z,GetMapZones(c))).."/"..QuestHelper:HighlightText(x*100)..","..QuestHelper:HighlightText(y*100)
-      else
-        name = "User Objective: "..QuestHelper:HighlightText(cat).."/"..QuestHelper:HighlightText(what)
-      end
-      
-      QuestHelper.user_objectives[objective] = name
-      QuestHelper:AddObjectiveWatch(objective, name)
-      
-      QuestHelper:TextOut("Created: "..name)
-      return
-    else
-      QuestHelper:TextOut("I don't know how to do that, yet.")
-      return
-    end
-  end
-  
-  QuestHelper:TextOut("I don't know what you want.")
-end
-
-SLASH_QuestHelper1 = "/qh"
-SLASH_QuestHelper2 = "/find"
-
-SlashCmdList["QuestHelper"] = slashHelper
+SLASH_QuestHelperFind1 = "/qhfind"
+SLASH_QuestHelperFind2 = "/find"
+SlashCmdList["QuestHelperFind"] = function (text) QuestHelper:PerformSearch(text) end
