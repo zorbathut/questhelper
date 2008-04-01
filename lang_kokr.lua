d3cf738122f7b1220626bffb4bc1fca0b113610f
J. P Kim <jpkim56@gmail.com>
2008-04-01 10:32:42 -0400

    Here is translated lang_kokr.lua file.

--- lang_kokr.lua
+++ lang_kokr.lua
@@ -2,131 +2,140 @@
 
 QuestHelper_Translations.koKR =
  {
-  --[[ THIS FILE NEEDS TO BE TRANSLATED; REMOVE THIS LINE WHEN COMPLETED.
   
   -- Messages used when starting.
-  LOCALE_ERROR = "The locale of your saved data doesn't match the locale of your WoW client.",
-  ZONE_LAYOUT_ERROR = "I'm refusing to run, out of fear of corrupting your saved data. "..
-                      "Please wait for a patch that will be able to handle the new zone layout.",
-  DOWNGRADE_ERROR = "Your saved data isn't compatible with this version of QuestHelper. "..
-                    "Use a new version, or delete your saved variables.",
-  HOME_NOT_KNOWN = "Your home isn't known. When you get a chance, please talk to your innkeeper and reset it.",
+  LOCALE_ERROR = "저장된 데이터의 로케일이 Wow 클라이언트의 로케일과 맞지 않습니다.",
+  ZONE_LAYOUT_ERROR = "세이브된 데이터와 충돌의 위험이 있기 때문에 애드온을 실행하지 않습니다. "..
+                      "새로운 지역을 처리 할 수 있는 패치가 나올때까지 기다려주세요.",
+  DOWNGRADE_ERROR = "저장된 데이터는 이 버전의 QuestHelper와 맞지 않습니다. "..
+                    "새로운 버전을 사용하거나, 저장된 데이터를 삭제하세요.",
+  HOME_NOT_KNOWN = "귀환 장소를 알 수 없습니다. 기회가 될 때, 여관주인에게 말을 걸어 재설정 하세요..",
   
   -- This text is only printed for the enUS client, don't worry about translating it.
   ALTERED_INDEX = "!!! QuestHelper_IndexLookup entry needs update: [%Q1] = {%2, %3, %4}",
   ALTERED_ZONE = "!!! QuestHelper_Zones entry needs update: [%1][%2] = %Q3 -- was %4",
   
   -- Route related text.
-  ROUTES_CHANGED = "The flight routes for your character have been altered.",
-  HOME_CHANGED = "Your home has been changed.",
-  TALK_TO_FLIGHT_MASTER = "Please talk to the local flight master.",
-  TALK_TO_FLIGHT_MASTER_COMPLETE = "Thanks.",
-  WILL_RESET_PATH = "Will reset pathing information.",
+  ROUTES_CHANGED = "당신의 이동 경로가 변경되었습니다.",
+  HOME_CHANGED = "귀환 장소가 변경되었습니다.",
+  TALK_TO_FLIGHT_MASTER = "이 지역의 비행 조련사에게 이야기 하세요.",
+  TALK_TO_FLIGHT_MASTER_COMPLETE = "완료",
+  WILL_RESET_PATH = "이동 정보가 재설정 됩니다.",
   
   -- Locale switcher.
-  LOCALE_LIST_BEGIN = "Available Locales:",
-  LOCALE_CHANGED = "Locale changed to: %h1",
-  LOCALE_UNKNOWN = "Locale %h1 isn't known.",
+  LOCALE_LIST_BEGIN = "이용 가능한 로케일 :",
+  LOCALE_CHANGED = "로케일이 변경되었음 : %h1",
+  LOCALE_UNKNOWN = "%h1 로케일을 찾을 수 없음.",
   
   -- Words used for objectives.
-  SLAY_VERB = "Slay",
-  ACQUIRE_VERB = "Acquire",
+  SLAY_VERB = "죽여라",
+  ACQUIRE_VERB = "획득하라",
   
-  OBJECTIVE_REASON = "%1 %h2 for quest %h3.", -- %1 is a verb, %2 is a noun (item or monster)
-  OBJECTIVE_REASON_FALLBACK = "%h1 for quest %h2.",
-  OBJECTIVE_REASON_TURNIN = "Turn in quest %h1.",
-  OBJECTIVE_PURCHASE = "Purchase from %h1.",
-  OBJECTIVE_TALK = "Talk to %h1.",
-  OBJECTIVE_SLAY = "Slay %h1.",
-  OBJECTIVE_LOOT = "Loot %h1.",
+  OBJECTIVE_REASON = "%h3 퀘스트 : %h2 %1", -- %1 is a verb, %2 is a noun (item or monster)
+  OBJECTIVE_REASON_FALLBACK = "%h2 퀘스트 : %h1.",
+  OBJECTIVE_REASON_TURNIN = "퀘스트 %h1.",
+  OBJECTIVE_PURCHASE = "%h1으로부터 구입하라.",
+  OBJECTIVE_TALK = "%h1에게 말하라.",
+  OBJECTIVE_SLAY = "%h1을 죽여라.",
+  OBJECTIVE_LOOT = "%h1을 루팅하라.",
   
-  ZONE_BORDER = "%1/%2 border",
+  ZONE_BORDER = "%1/%2 지역",
   
   -- Stuff used in objective menus.
-  PRIORITY = "Priority",
-  PRIORITY1 = "Highest",
-  PRIORITY2 = "High",
-  PRIORITY3 = "Normal",
-  PRIORITY4 = "Low",
-  PRIORITY5 = "Lowest",
-  SHARING = "Sharing",
-  ENABLE = "Enable",
-  DISABLE = "Disable",
-  IGNORE = "Ignore",
+  PRIORITY = "중요도",
+  PRIORITY1 = "매우 높음",
+  PRIORITY2 = "높음",
+  PRIORITY3 = "보통",
+  PRIORITY4 = "낮음",
+  PRIORITY5 = "매우 낮음",
+  SHARING = "공유",
+  ENABLE = "이용 가능",
+  DISABLE = "이용 불가",
+  IGNORE = "무시",
   
-  IGNORED_PRIORITY_TITLE = "The selected priority would be ignored.",
-  IGNORED_PRIORITY_FIX = "Apply same priority to the blocking objectives.",
-  IGNORED_PRIORITY_IGNORE = "I'll set the priorities myself.",
+  IGNORED_PRIORITY_TITLE = "선택된 중요도는 무시됩니다.",
+  IGNORED_PRIORITY_FIX = "목적들을 막기위해 같은 중요도를 적용하세요.",
+  IGNORED_PRIORITY_IGNORE = "프로퍼티들을 재설정 합니다.",
   
   -- Custom objectives.
-  RESULTS_TITLE = "Search Results",
-  NO_RESULTS = "There aren't any!",
-  CREATED_OBJ = "Created: %1",
-  REMOVED_OBJ = "Removed: %1",
-  USER_OBJ = "User Objective: %h1",
-  UNKNOWN_OBJ = "I don't know where you should go for that objective.",
+  RESULTS_TITLE = "검색 결과",
+  NO_RESULTS = "결과를 찾을 수 없습니다.",
+  CREATED_OBJ = "생성 : %1",
+  REMOVED_OBJ = "삭제 : %1",
+  USER_OBJ = "유저 목적 : %h1",
+  UNKNOWN_OBJ = "목적 달성을 위해 어디로 가야하는지 알 수가 없습니다.",
   
-  SEARCHING_STATE = "Searching: %1",
-  SEARCHING_LOCAL = "Local %1",
-  SEARCHING_STATIC = "Static %1",
-  SEARCHING_ITEMS = "Items",
-  SEARCHING_NPCS = "NPCs",
-  SEARCHING_ZONES = "Zones",
-  SEARCHING_DONE = "Done!",
+  SEARCHING_STATE = "검색 : %1",
+  SEARCHING_LOCAL = "%1 구역",
+  SEARCHING_STATIC = "%1 전역",
+  SEARCHING_ITEMS = "아이템",
+  SEARCHING_NPCS = "NPC들",
+  SEARCHING_ZONES = "지역",
+  SEARCHING_DONE = "완료!",
   
   -- Shared objectives.
-  PEER_TURNIN = "Wait for %h1 to turn in %h2.",
-  PEER_LOCATION = "Help %h1 reach a location in %h2.",
-  PEER_ITEM = "Help %1 to acquire %h2.",
-  PEER_OTHER = "Assist %1 with %h2.",
+  PEER_TURNIN = "%h2로 진행하기 위해 %h1을 기다리세요.",
+  PEER_LOCATION = "%h1을 도와 %h2 지역으로 이동하라.",
+  PEER_ITEM = "%1을 도와 %h2를 획득하라.",
+  PEER_OTHER = "%h2와 같이 %1을 도와라.",
   
-  PEER_NEWER = "%h1 is using a newer protocol version. It might be time to upgrade.",
-  PEER_OLDER = "%h1 is using an older protocol version.",
+  PEER_NEWER = "%h1은 새로운 버전의 프로토콜을 이용하였습니다. 업그레이드가 필요할 지도 모릅니다.",
+  PEER_OLDER = "%h1은 구버전의 프로토콜을 이용하였습니다.",
   
-  UNKNOWN_MESSAGE = "Unknown message type '%1' from '%2'.",
+  UNKNOWN_MESSAGE = "'%2'로 부터 받은 '%1'은 알 수 없는 메세지 타입.",
   
   -- Hidden objectives.
-  HIDDEN_TITLE = "Hidden Objectives",
-  HIDDEN_NONE = "There are no objectives hidden from you.",
-  DEPENDS_ON_SINGLE = "Depends on '%1'.",
-  DEPENDS_ON_COUNT = "Depends on %1 hidden objectives.",
-  FILTERED_LEVEL = "Filtered due to level.",
-  FILTERED_ZONE = "Filtered due to zone.",
-  FILTERED_COMPLETE = "Filtered due to completeness.",
-  FILTERED_USER = "You requested this objective be hidden.",
-  FILTERED_UNKNOWN = "Don't know how to complete.",
+  HIDDEN_TITLE = "감춘 목적",
+  HIDDEN_NONE = "감춘 목적이 존재하지 않습니다.",
+  DEPENDS_ON_SINGLE = "'%1'에 의존함.",
+  DEPENDS_ON_COUNT = "%1은 감춘 목적에 의존함.",
+  FILTERED_LEVEL = "레벨에 따른 분류.",
+  FILTERED_ZONE = "지역에 따른 분류.",
+  FILTERED_COMPLETE = "완료 상태에 따른 분류.",
+  FILTERED_USER = "이 목적을 감추길 요청하였음.",
+  FILTERED_UNKNOWN = "완료 방법을 알 수 없음.",
   
-  HIDDEN_SHOW = "Show.",
-  DISABLE_FILTER = "Disable filter: %1",
-  FILTER_DONE = "done",
-  FILTER_ZONE = "zone",
-  FILTER_LEVEL = "level",
+  HIDDEN_SHOW = "보기.",
+  DISABLE_FILTER = "필터 비활성화 : %1",
+  FILTER_DONE = "완료",
+  FILTER_ZONE = "지역",
+  FILTER_LEVEL = "레벨",
   
   -- Nagging. (This is incomplete, only translating strings for the non-verbose version of the nag command that appears at startup.)
   NAG_SINGLE = "1 %2", -- %1 == count (will be 1), %2 == what
   NAG_PLURAL = "%1 %s2",
   
-  NAG_MULTIPLE_NEW = "You have information on %h1 new and %h2 updated %h(%s3).",
-  NAG_SINGLE_NEW = "You have new information on %h1.",
-  NAG_ADDITIONAL = "You have additional information on %h1.",
+  NAG_MULTIPLE_NEW = "%h1과 %h2의 정보 그리고, %h(%s3)지역의 정보를 갱신하였습니다.",
+  NAG_SINGLE_NEW = "%h1의 새로운 정보를 얻었습니다.",
+  NAG_ADDITIONAL = "%h1의 추가 정보를 얻었습니다.",
   
-  NAG_NOT_NEW = "You don't have any information not already in the static database.",
-  NAG_NEW = "You might consider sharing your data so that others may benefit.",
+  NAG_NOT_NEW = "당신의 전역 데이터베이스에 어떠한 정보도 가지고 있지 않습니다.",
+  NAG_NEW = "다른 사람들에게 도움이 될 지도 모르니 당신의 데이터 공유를 고려해 보십시오.",
   
-  NAG_FP = "flight master",
-  NAG_QUEST = "quest",
-  NAG_ROUTE = "flight route",
-  NAG_ITEM_OBJ = "item objective",
-  NAG_OBJECT_OBJ = "object objective",
-  NAG_MONSTER_OBJ = "monster objective",
-  NAG_EVENT_OBJ = "event objective",
-  NAG_REPUTATION_OBJ = "reputation objective",
+  NAG_FP = "비행 조련사",
+  NAG_QUEST = "퀘스트",
+  NAG_ROUTE = "이동 경로",
+  NAG_ITEM_OBJ = "아이템 목적",
+  NAG_OBJECT_OBJ = "목표 목적",
+  NAG_MONSTER_OBJ = "몬스터 목적",
+  NAG_EVENT_OBJ = "이벤트 목적",
+  NAG_REPUTATION_OBJ = "평판 목적",
   
   -- Stuff used by dodads.
-  PEER_PROGRESS = "%1's progress:",
-  TRAVEL_ESTIMATE = "Estimated travel time:",
+  PEER_PROGRESS = "%1의 진행상황 :",
+  TRAVEL_ESTIMATE = "계산된 여행 시간 :",
   TRAVEL_ESTIMATE_VALUE = "%t1",
-  WAYPOINT_REASON = "Visit %h1 en route to:"
+  WAYPOINT_REASON = "이곳을 통하여 %h1을 방문하라 :"
   
-  END COMMENT BLOCK; REMOVE THIS LINE WHEN COMPLETED ]]
+ }
+ 
+ QuestHelper_TranslationFunctions.koKR =
+ {
+  ["s"] = function(data)
+    if string.find(data, "s$") then
+        return data.."들"
+      else
+        return data.."들"
+      end
+    end,
  }
