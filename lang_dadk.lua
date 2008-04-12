456b36f4ae9ef9dae1f2bc2f3ff21f93e22a3056
Casper <dan11372@soda.dk>
2008-04-12 15:25:13 -0400

    I've taken the time to translate the lang_enus.lua fil to Danish

---
+++ lang_dadk.lua
@@ -0,0 +1,126 @@
+QuestHelper_Translations.daDK =
+ {
+  -- Messages used when starting.
+  LOCALE_ERROR = "Sproget i dine gemte data stemmer ikke med sprogt for til WoW klient.",
+  ZONE_LAYOUT_ERROR = "Jeg nægter at løbe, ud af frygt for, at ødelægge dine gemte data. "..
+                      "Vent venligt på en ny patch, der vil være i stand til at håndere det nye zone layout.",
+  DOWNGRADE_ERROR = "Dine gemte data er ikke kompatible med denne version af QuestHelper. "..
+                    "Brug en nyere version eller slet dine gemte variabler.",
+  HOME_NOT_KNOWN = "Din hjemmelokation er ikke kendt. Kontakt en kroejer for at resætte den.",
+  
+  -- This text is only printed for the enUS client, don't worry about translating it.
+  ALTERED_INDEX = "!!! QuestHelper_IndexLookup entry needs update: [%Q1] = {%2, %3, %4}",
+  ALTERED_ZONE = "!!! QuestHelper_Zones entry needs update: [%1][%2] = %Q3 -- was %4",
+  
+  -- Route related text.
+  ROUTES_CHANGED = "Dine flyveruter for din karakter er blevet ændret.",
+  HOME_CHANGED = "Din hjemmelokation er ændret.",
+  TALK_TO_FLIGHT_MASTER = "Venligst tal med din lokale flyvemester.",
+  TALK_TO_FLIGHT_MASTER_COMPLETE = "Tak.",
+  WILL_RESET_PATH = "Pathing information vil blive resat.",
+  
+  -- Locale switcher.
+  LOCALE_LIST_BEGIN = "Tilgændelige sprog:",
+  LOCALE_CHANGED = "Sprog er ændret til: %h1",
+  LOCALE_UNKNOWN = "Sproget %h1 er ikke kendt.",
+  
+  -- Words used for objectives.
+  SLAY_VERB = "Dræb",
+  ACQUIRE_VERB = "Få fat i",
+  
+  OBJECTIVE_REASON = "%1 %h2 til quest %h3.", -- %1 er et verbum, %2 er et navneord (genstand eller monster)
+  OBJECTIVE_REASON_FALLBACK = "%h1 til questen %h2.",
+  OBJECTIVE_REASON_TURNIN = "Aflever questen %h1.",
+  OBJECTIVE_PURCHASE = "Køb fra %h1.",
+  OBJECTIVE_TALK = "Tal me %h1.",
+  OBJECTIVE_SLAY = "Dræb %h1.",
+  OBJECTIVE_LOOT = "Saml %h1.",
+  
+  ZONE_BORDER = "%1/%2 grænse",
+  
+  -- Stuff used in objective menus.
+  PRIORITY = "Prioritet",
+  PRIORITY1 = "Højeste",
+  PRIORITY2 = "Høj",
+  PRIORITY3 = "Normal",
+  PRIORITY4 = "Lav",
+  PRIORITY5 = "Laveste",
+  SHARING = "Del",
+  ENABLE = "Aktiver",
+  DISABLE = "Deaktiver",
+  IGNORE = "Ignore",
+  
+  IGNORED_PRIORITY_TITLE = "Den valgte prioritet bliver ignoreret.",
+  IGNORED_PRIORITY_FIX = "Sæt samme prioritet til de(t) blokerede objekt(er).",
+  IGNORED_PRIORITY_IGNORE = "Jeg sætter selv prioriten.",
+  
+  -- Custom objectives.
+  RESULTS_TITLE = "Søgeresultater",
+  NO_RESULTS = "Der er ingen!",
+  CREATED_OBJ = "Oprettet: %1",
+  REMOVED_OBJ = "Fjernet: %1",
+  USER_OBJ = "Bruger objekivet: %h1",
+  UNKNOWN_OBJ = "Jeg ved ikke, hvor du skal gå hen til dette objektiv.",
+  
+  SEARCHING_STATE = "Søger: %1",
+  SEARCHING_LOCAL = "Sprog %1",
+  SEARCHING_STATIC = "Statisk %1",
+  SEARCHING_ITEMS = "Genstand",
+  SEARCHING_NPCS = "NPCer",
+  SEARCHING_ZONES = "Zoner",
+  SEARCHING_DONE = "Færdig!",
+  
+  -- Shared objectives.
+  PEER_TURNIN = "Vent på %h1 for at aflvere %h2.",
+  PEER_LOCATION = "Hjælp %h1 med at nå et sted i %h2.",
+  PEER_ITEM = "Hjælp %1 med at få fat i %h2.",
+  PEER_OTHER = "Assistér %1 med %h2.",
+  
+  PEER_NEWER = "%h1 bruger en nyere protokol version. Måske på tide at opgradere.",
+  PEER_OLDER = "%h1 bruger en ældre protokol version.",
+  
+  UNKNOWN_MESSAGE = "Ukendt beskedstype '%1' fra '%2'.",
+  
+  -- Hidden objectives.
+  HIDDEN_TITLE = "Gemte Objekter",
+  HIDDEN_NONE = "Der er ingen gemte objekter for dig.",
+  DEPENDS_ON_SINGLE = "Kommer an på '%1'.",
+  DEPENDS_ON_COUNT = "Kommer an på %1 gemte objecter.",
+  FILTERED_LEVEL = "Filtreret grundet level.",
+  FILTERED_ZONE = "Filtreret grundet område.",
+  FILTERED_COMPLETE = "Filtreret grundet afsluttet.",
+  FILTERED_USER = "Du har anmodet om, at dette opjektiv bliver gemt.",
+  FILTERED_UNKNOWN = "Ved ikke hvordan det færdiggøres.",
+  
+  HIDDEN_SHOW = "Vis.",
+  DISABLE_FILTER = "Lukkede filtre: %1",
+  FILTER_DONE = "færdig",
+  FILTER_ZONE = "område",
+  FILTER_LEVEL = "level",
+  
+  -- Nagging. (This is incomplete, only translating strings for the non-verbose version of the nag command that appears at startup.)
+  NAG_SINGLE = "1 %2", -- %1 == count (will be 1), %2 == what
+  NAG_PLURAL = "%1 %s2",
+  
+  NAG_MULTIPLE_NEW = "Du har ny information omkring %h1 og %h2 opdaterede %h(%s3).",
+  NAG_SINGLE_NEW = "Du har ny information til %h1.",
+  NAG_ADDITIONAL = "Du har yderligere information til %h1.",
+  
+  NAG_NOT_NEW = "Du har ingen information, som ikke allerede er i den statiske database.",
+  NAG_NEW = "Du bør overveje, at dele dine data, så andre kan gøre brug af dem.",
+  
+  NAG_FP = "Flyve Mester",
+  NAG_QUEST = "quest",
+  NAG_ROUTE = "flyverute",
+  NAG_ITEM_OBJ = "item objektiv",
+  NAG_OBJECT_OBJ = "objekt objektiv",
+  NAG_MONSTER_OBJ = "monster objektiv",
+  NAG_EVENT_OBJ = "begivenheds objektiv",
+  NAG_REPUTATION_OBJ = "Omdømme objektiv",
+  
+  -- Stuff used by dodads.
+  PEER_PROGRESS = "%1's fremskridt:",
+  TRAVEL_ESTIMATE = "anslået rejse tid:",
+  TRAVEL_ESTIMATE_VALUE = "%t1",
+  WAYPOINT_REASON = "Besøg %h1 på vej til:"
+ }
