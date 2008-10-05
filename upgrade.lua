bf96ee6200a0e7cd5f112d533dd19f8eed59ce81
zorba <zorba-questhelper-git@pavlovian.net>
2008-10-05 08:40:21 -0700

    remove that extra parameter from ConvertFaction also

--- upgrade.lua
+++ upgrade.lua
@@ -288,11 +288,11 @@ local function QuestHelper_ConvertPositionList(list)
   end
 end
 
-local function QuestHelper_ConvertFaction(locale, faction)
+local function QuestHelper_ConvertFaction(faction)
   if faction == 1 or faction == "Alliance" or faction == FACTION_ALLIANCE then return 1
   elseif faction == 2 or faction == "Horde" or faction == FACTION_HORDE then return 2
   else
-    assert(false, "Unknown faction: "..locale.."/'"..faction.."'")
+    assert(false, "Unknown faction: "..faction.."'")
   end
 end
 
@@ -442,20 +442,20 @@ function QuestHelper_UpgradeDatabase(data)
     -- Also converting factions to numbers, 1 for Alliance, 2 for Horde.
     local replacement = {}
     for faction, dat in pairs(data.QuestHelper_Quests) do
-      replacement[QuestHelper_ConvertFaction(data.QuestHelper_Locale, faction)] = dat
+      replacement[QuestHelper_ConvertFaction(faction)] = dat
     end
     data.QuestHelper_Quests = replacement
     
     replacement = {}
     if data.QuestHelper_FlightInstructors then for faction, dat in pairs(data.QuestHelper_FlightInstructors) do
-      replacement[QuestHelper_ConvertFaction(data.QuestHelper_Locale, faction)] = dat
+      replacement[QuestHelper_ConvertFaction(faction)] = dat
     end end
     data.QuestHelper_FlightInstructors = replacement
     
     for cat, list in pairs(data.QuestHelper_Objectives) do
       for name, obj in pairs(list) do
         if obj.faction then
-          obj.faction = QuestHelper_ConvertFaction(data.QuestHelper_Locale, obj.faction)
+          obj.faction = QuestHelper_ConvertFaction(obj.faction)
         end
       end
     end
