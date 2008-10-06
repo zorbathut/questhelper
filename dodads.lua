36defa15a7bc70f464775e154a363207f8e1a426
zorba <zorba-questhelper-git@pavlovian.net>
2008-10-06 12:31:55 -0700

    Minor bug in showing updated quest order

--- dodads.lua
+++ dodads.lua
@@ -645,7 +645,7 @@ function QuestHelper:CreateMipmapDodad()
       
       self:Show()
       
-      if self.recalc_timeout == 0 and not QuestHelper.graph_in_limbo then
+      if self.recalc_timeout <= 0 and not QuestHelper.graph_in_limbo then
         self.recalc_timeout = 50
         
         self.objective = self:NextObjective()
