44615069eb8cf6499d7898f421a28f8ae1ecacb4
zorba <zorba-questhelper-git@pavlovian.net>
2008-11-04 02:28:55 -0800

    arglbargl

--- AchievementNotifier/AchievementNotifier.lua
+++ AchievementNotifier/AchievementNotifier.lua
@@ -52,70 +52,114 @@ end
 function TO(text)
   DEFAULT_CHAT_FRAME:AddMessage(string.format("|cffffcc00AchievementNotifier: |r%s", text))
 end
-                                
-local function getAchievementDB()
+
+local crittypes = {}
+
+QuestHelper_ZorbaForgotToRemoveThis = {}
+
+local function registerAchievement(id, db)
+  if db.achievements[id] then return end
+  
+  local _, title, _, complete = GetAchievementInfo(id)
+  --TO(string.format("Registering %d (%s)", id, title))
+  local prev = GetPreviousAchievement(id)
+  
+  db.achievements[id] = {
+    previous = prev,
+    compete = complete,
+    name = title,
+    criterialist = {}
+  }
+  local dbi = db.achievements[id]
+  
+  if prev then
+    registerAchievement(prev, db)
+  end
+  
+  --[[
+  local known = {}
+  known[0] = true
+  known[1] = true
+  known[7] = true
+  known[8] = true
+  known[29] = true
+  known[110] = true
+  ]]
+  
+  local critcount = GetAchievementNumCriteria(id)
+  --TO(string.format("%d criteria", crit))
+  for i = 1, critcount do
+    local crit_name, crit_type, crit_complete, crit_quantity, crit_reqquantity, _, _, crit_asset, _, crit_id = GetAchievementCriteriaInfo(id, i)
+    --[[local mega = {GetAchievementCriteriaInfo(id, i)}
+    if mega[2] == 0 then
+      TO(string.format("%s: %s, %s, %s, %s, %s, %s, %s, %s, %s, %s", tostring(title), tostring(mega[1]), tostring(mega[2]), tostring(mega[3]), tostring(mega[4]), tostring(mega[5]), tostring(mega[6]), tostring(mega[7]), tostring(mega[8]), tostring(mega[9]), tostring(mega[10])))
+    end]]
+    
+    if not QuestHelper_ZorbaForgotToRemoveThis[string.format("%d", crit_type)] then QuestHelper_ZorbaForgotToRemoveThis[string.format("%d", crit_type)] = {} end
+    QuestHelper_ZorbaForgotToRemoveThis[string.format("%d", crit_type)][title .. " --- " .. mega[1]] = crit_asset
+    
+    table.insert(dbi.criterialist, crit_id)
+    assert(not db.criteria[crit_id])
+    crittypes[crit_type] = (crittypes[crit_type] or 0) + 1
+    db.criteria[crit_id] = {
+      name = crit_name,
+      type = crit_type,
+      complete = crit_complete,
+      progress = crit_quantity,
+      progress_total = crit_reqquantity,
+      asset = crit_asset,
+    }
+  end
+end
+  
+function getAchievementDB()
   local db = {}
   db.achievements = {}
   db.criteria = {}
-  TO(string.format("ASS DB"))
   
-  local function registerAchievement(id, db)
-    if db[id] then return end
-    
-    db[id] = {}
-    dbi = db[id]
-    
-    local _, title, _, complete = GetAchievementInfo(id)
-    --TO(string.format("Registering %d (%s)", id, title))
-    local prev = GetPreviousAchievement(id)
-    dbi.previous = prev
-    dbi.complete = complete
-    dbi.name = title
-    dbi.criterialist = {}
-    
-    if prev then
-      registerAchievement(prev, db)
-    end
-    
-    --[[
-    local known = {}
-    known[0] = true
-    known[1] = true
-    known[7] = true
-    known[8] = true
-    known[29] = true
-    known[110] = true
-    ]]
-    
-    local crit = GetAchievementNumCriteria(id)
-    --TO(string.format("%d criteria", crit))
-    for i = 1, crit do
-      local crit_name, crit_type, crit_complete, crit_quantity, crit_reqquantity, _, _, crit_asset, _, crit_id = GetAchievementCriteriaInfo(id, i)
-      --[[if not known[mega[2] ] then
-        TO(string.format("%s: %s, %s, %s, %s, %s, %s, %s, %s, %s, %s", tostring(title), tostring(mega[1]), tostring(mega[2]), tostring(mega[3]), tostring(mega[4]), tostring(mega[5]), tostring(mega[6]), tostring(mega[7]), tostring(mega[8]), tostring(mega[9]), tostring(mega[10])))
-      end]]
-      table.insert(dbi.criterialist, crit_id)
-      assert(not db.criteria[crit_id])
-      db.criteria[crit_id] = {
-        name = crit_name,
-        type = crit_type,
-        complete = crit_complete,
-        progress = crit_quantity,
-        progress_total = crit_reqquantity,
-        asset = crit_asset,
-      }
-    end
-  end
+  crittypes = {}
+  QuestHelper_ZorbaForgotToRemoveThis = {}
   
-  -- Type 0 is a monster kill
-  --  assetID is the monster ID
-  -- Type 1 is some sort of PvP objective? "holding bases", "controlling flags"
-  -- Type 7 is weapon skill
-  -- Type 8 means another achievement
-  --  assetID is achievement ID
-  -- Type 29 is "craft by cooking"
-  -- Type 110 is "use mistletoe on" (what)
-  -- I'm not bothering with more right now
+  -- 0 is a monster kill, asset is the monster ID
+  -- 1 is winning PvP objectives in a thorough manner (holding all bases, controlling all flags)
+  -- 7 is weapon skill, asset is probably a skill ID of some sort
+  -- 8 is another achievement, asset is achievement ID
+  -- 9 is completing quests globally
+  -- 10 is completing a daily quest every day
+  -- 11 is completing quests in specific areas
+  -- 14 is completing daily quests
+  -- 27 is a quest, asset is quest ID
+  -- 28 is getting a spell cast on you, asset is a spell ID
+  -- 29 is casting a spell (often crafting), asset is a spell ID
+  -- 30 is PvP objectives (flags, assaulting, defending)
+  -- 31 is PvP kills in battleground PvP locations
+  -- 32 is winning ranked arena matches in specific locations (asset is probably a location ID)
+  -- 34 is the Squashling (owning a specific pet?), asset is the spell ID
+  -- 35 is PvP kills while under the influence of something
+  -- 36 is acquiring items (soulbound), asset is an item ID
+  -- 37 is winning arenas
+  -- 41 is eating or drinking a specific item, asset is item ID
+  -- 42 is fishing things up, asset is item ID
+  -- 43 is exploration, asset is a location ID?
+  -- 45 is purchasing 7 bank slots
+  -- 46 is exalted rep, asset is presumably some kind of faction ID
+  -- 47 is 5 reputations to exalted
+  -- 49 is equipping items, asset is a slot ID (quality is presumably encoded into flags)
+  -- 52 is killing specific classes of player
+  -- 53 is kill-a-given-race, asset is race ID?
+  -- 54 is using emotes on targets, asset ID is likely the emote ID
+  -- 56 is being a wrecking ball in Alterac Valley
+  -- 62 is getting gold from quest rewards
+  -- 67 is looting gold
+  -- 68 is reading books
+  -- 70 is killing players in world PvP locations
+  -- 72 is fishing things from schools or wreckage
+  -- 73 is killing Mal'Ganis on Heroic. Why? Who can say.
+  -- 75 is obtaining mounts
+  -- 109 is fishing, either in general or in specific locations
+  -- 110 is casting spells on specific targets, asset ID is the spell ID
+  -- 112 is learning cooking recipes
+  -- 113 is honorable kills
   
   for _, catid in pairs(GetCategoryList()) do
     for d = 1, GetCategoryNumAchievements(catid) do
@@ -134,7 +178,33 @@ local function activate(newinstance, oldinstance)
   newinstance.AchievementDB = getAchievementDB() -- 'coz we're lazy
 end
 
+local needsUpdate = true
+
+local function OnEvent(frame, event)
+  TO(event)
+  needsUpdate = true
+end
+
+local function OnUpdate()
+  if needsUpdate then
+    local newAchievementDB = getAchievementDB()
+    TO("update!")
+    needsUpdate = false
+  end
+end
+
 AchievementNotifier.frame = CreateFrame("Frame")
+
+AchievementNotifier.frame:UnregisterAllEvents()
+AchievementNotifier.frame:RegisterEvent("CRITERIA_UPDATE")
+AchievementNotifier.frame:RegisterEvent("ACHIEVEMENT_EARNED")
+AchievementNotifier.frame:RegisterEvent("ZONE_CHANGED")
+AchievementNotifier.frame:RegisterEvent("ZONE_CHANGED_INDOORS")
+AchievementNotifier.frame:RegisterEvent("MINIMAP_PING")
+AchievementNotifier.frame:SetScript("OnEvent", OnEvent)
+AchievementNotifier.frame:SetScript("OnUpdate", OnUpdate)
+
+AchievementNotifier.frame:Show()
 
 --SlashCmdList["ACHIEVEMENT_NOTIFIER"] = SlashCommand;
 --ACHIEVEMENT_NOTIFIER1 = "/an";
@@ -161,4 +231,11 @@ DongleStub:Register(AchievementNotifier, activate)
   }
 }
 
+
+notifications/hooks:
+* Criteria changed
+* Progress made (subset of criteria changed)
+* Achievement completed (subset of progress made)
+
+* New achievement
 ]]
