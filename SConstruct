0eedf487e5bfaff0cd85c9a27fcaa1123a9fc29b
zorba <zorba-questhelper-git@pavlovian.net>
2009-01-04 10:58:25 -0800

    progress, as usual

--- SConstruct
+++ SConstruct
@@ -2,5 +2,7 @@
 def command(name, deps, line):
   AlwaysBuild(Alias(name, deps, line))
 
+command("build", ["01", "02"], None)
 
 command("01", [], "./build_01_copy")
+command("02", [], "./build_02_decompress")
--- utility.py
+++ utility.py
@@ -3,3 +3,16 @@ import subprocess
 
 def exe(line):
   return str(subprocess.Popen(line.split(" "), stdout=subprocess.PIPE).communicate()[0], "ascii")
+
+def exe_rv(line):
+  ldat = line.split(" ")
+  if line.find("BASHHACK") != -1:
+    ldat = ["bash", "-c", line.split(" ", 1)[1]]
+  
+  sp = subprocess.Popen(ldat, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  so, se = sp.communicate()
+  so = str(so, "ascii")
+  se = str(se, "ascii")
+  rv = sp.returncode
+  assert(rv != None)
+  return so, se, rv
