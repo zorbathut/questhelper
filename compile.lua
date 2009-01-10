76188bcb92849bc4276770385b7884556d98ea57
zorba <zorba-questhelper-git@pavlovian.net>
2009-01-10 12:39:51 -0800

    I bet this is making pretty chunks of picture now

--- compile.lua
+++ compile.lua
@@ -6,15 +6,22 @@ package.loadlib("/home/zorba/build/libcompile_core.so", "init")()   -- this will
 
 os.execute("rm -rf intermed")
 
-greet()
-slice_loc("testing")
-if true then return end
+math.umod = function (val, med)
+  if val < 0 then
+    return math.mod(val + math.ceil(-val / med + 10) * med, med)
+  else
+    return math.mod(val, med)
+  end
+end
 
--- package.loadlib("/home/zorba/build/libcompile_core.so", "init")()
--- greet()
+local zone_image_chunksize = 1024
+local zone_image_descale = 4
+local zone_image_outchunk = zone_image_chunksize / zone_image_descale
 
 local chainhead = ChainBlock_Create(nil,
   function () return {
+    zonecolors = {},
+    
     Data = function (self, key, subkey, value, Output)
       dat = loadfile(key)()
       for k, v in pairs(dat.QuestHelper_Errors) do
@@ -23,86 +30,140 @@ local chainhead = ChainBlock_Create(nil,
           Output(d.local_version, nil, d, "error")
         end
       end
+      
+      for verchunk, v in pairs(dat.QuestHelper_Collector) do
+        if string.find(verchunk, "enUS") then -- hacky hacky
+          -- zones!
+          if v.zone then for zname, zdat in pairs(v.zone) do
+            for _, key in pairs({"border", "update"}) do
+              if zdat[key] then for idx, chunk in pairs(zdat[key]) do
+                if math.mod(#chunk, 11) ~= 0 then print("Non-splittable chunk, " .. tostring(#chunk)) break end -- hmmmm
+                assert(math.mod(#chunk, 11) == 0, tostring(#chunk))
+                for point = 1, #chunk, 11 do
+                  local pos = {slice_loc(string.sub(chunk, point, point + 10))}
+                  if not self.zonecolors[zname] then
+                    local r, g, b = math.ceil(math.random(32, 255)), math.ceil(math.random(32, 255)), math.ceil(math.random(32, 255))
+                    self.zonecolors[zname] = r * 65536 + g * 256 + b
+                  end
+                  pos.zonecolor = self.zonecolors[zname]
+                  if pos[1] and pos[2] and pos[3] then  -- These might be invalid if there are nils embedded in the string. They might still be useful with only one or two nils, but I've got a bunch of data and don't really need more.
+                    Output(string.format("%d@%d@%d", pos[1], math.floor(pos[2] / zone_image_chunksize), math.floor(pos[3] / zone_image_chunksize)), nil, pos, "zone")
+                  end
+                end
+              end end
+            end
+          end end
+        end
+      end
     end
   } end
 )
 
-local error_collater = ChainBlock_Create(chainhead,
-  function (key) return {
-    accum = {},
-    
-    Data = function (self, key, subkey, value, Output)
-      assert(value.local_version)
-      if not value.toc_version or value.local_version ~= value.toc_version then return end
-      local signature
-      if value.key ~= "crashes" then signature = value.key end
-      if not signature then signature = value.message end
-      local v = value.local_version
-      if not self.accum[v] then self.accum[v] = {} end
-      if not self.accum[v][signature] then self.accum[v][signature] = {count = 0, dats = {}, sig = signature, ver = v} end
-      self.accum[v][signature].count = self.accum[v][signature].count + 1
-      table.insert(self.accum[v][signature].dats, value)
-    end,
-    
-    Finish = function (self, Output)
-      for ver, chunk in pairs(self.accum) do
-        local tbd = {}
-        for _, v in pairs(chunk) do
-          table.insert(tbd, v)
-        end
-        table.sort(tbd, function(a, b) return a.count > b.count end)
-        for i, dat in pairs(tbd) do
-          dat.count_pos = i
-          Output("", nil, dat)
-        end
-      end
-    end
-  } end,
-  nil, "error"
-)
+--[[
+*****************************************************************
+Zone collation
+]]
 
 do
-  local function acuv(tab, ites)
-    local sit = ""
-    for _, v in pairs(ites) do
-      sit = sit .. string.format("%s: %s\n", tostring(v), tostring(tab[v]))
-      tab[v] = nil
-    end
-    return sit
-  end
-  local function keez(tab)
-    local rv = {}
-    for k, _ in pairs(tab) do
-      table.insert(rv, k)
-    end
-    return rv
-  end
-  
-  local error_writer = ChainBlock_Create(error_collater,
+  local zone_collector = ChainBlock_Create(chainhead,
     function (key) return {
-      Data = function (self, key, subkey, value, Output)
-        os.execute("mkdir -p intermed/error/" .. value.ver)
-        fil = io.open(string.format("intermed/error/%s/%03d-%05d.txt", value.ver, value.count_pos, value.count), "w")
-        fil:write(value.sig)
-        fil:write("\n\n\n\n")
-        
-        for _, tab in pairs(value.dats) do
-          local prefix = acuv(tab, {"message", "key", "toc_version", "local_version", "game_version", "locale", "timestamp", "mutation"})
-          local postfix = acuv(tab, {"stack", "addons"})
-          local midfix = acuv(tab, keez(tab))
-          
-          fil:write(prefix)
-          fil:write("\n")
-          fil:write(midfix)
-          fil:write("\n")
-          fil:write(postfix)
-          fil:write("\n\n\n")
-        end
-        
-        fil:close()
-      end
-    } end
+      imagepiece = Image(zone_image_outchunk, zone_image_outchunk),
+      
+      Data = function(self, key, subkey, value, Output)
+        self.imagepiece:set(math.floor(math.umod(value[2], zone_image_chunksize) / zone_image_descale), math.floor(math.umod(value[3], zone_image_chunksize) / zone_image_descale), value.zonecolor)
+      end,
+      
+      Finish = function(self, Output)
+        Output("", nil, {loc = key, image = imagepiece})
+      end,
+    } end,
+    nil, "zone"
   )
+end
+
+--[[
+*****************************************************************
+Error collation
+]]
+
+do
+  local error_collater = ChainBlock_Create(chainhead,
+    function (key) return {
+      accum = {},
+      
+      Data = function (self, key, subkey, value, Output)
+        assert(value.local_version)
+        if not value.toc_version or value.local_version ~= value.toc_version then return end
+        local signature
+        if value.key ~= "crashes" then signature = value.key end
+        if not signature then signature = value.message end
+        local v = value.local_version
+        if not self.accum[v] then self.accum[v] = {} end
+        if not self.accum[v][signature] then self.accum[v][signature] = {count = 0, dats = {}, sig = signature, ver = v} end
+        self.accum[v][signature].count = self.accum[v][signature].count + 1
+        table.insert(self.accum[v][signature].dats, value)
+      end,
+      
+      Finish = function (self, Output)
+        for ver, chunk in pairs(self.accum) do
+          local tbd = {}
+          for _, v in pairs(chunk) do
+            table.insert(tbd, v)
+          end
+          table.sort(tbd, function(a, b) return a.count > b.count end)
+          for i, dat in pairs(tbd) do
+            dat.count_pos = i
+            Output("", nil, dat)
+          end
+        end
+      end
+    } end,
+    nil, "error"
+  )
+
+  do
+    local function acuv(tab, ites)
+      local sit = ""
+      for _, v in pairs(ites) do
+        sit = sit .. string.format("%s: %s\n", tostring(v), tostring(tab[v]))
+        tab[v] = nil
+      end
+      return sit
+    end
+    local function keez(tab)
+      local rv = {}
+      for k, _ in pairs(tab) do
+        table.insert(rv, k)
+      end
+      return rv
+    end
+    
+    local error_writer = ChainBlock_Create(error_collater,
+      function (key) return {
+        Data = function (self, key, subkey, value, Output)
+          os.execute("mkdir -p intermed/error/" .. value.ver)
+          fil = io.open(string.format("intermed/error/%s/%03d-%05d.txt", value.ver, value.count_pos, value.count), "w")
+          fil:write(value.sig)
+          fil:write("\n\n\n\n")
+          
+          for _, tab in pairs(value.dats) do
+            local prefix = acuv(tab, {"message", "key", "toc_version", "local_version", "game_version", "locale", "timestamp", "mutation"})
+            local postfix = acuv(tab, {"stack", "addons"})
+            local midfix = acuv(tab, keez(tab))
+            
+            fil:write(prefix)
+            fil:write("\n")
+            fil:write(midfix)
+            fil:write("\n")
+            fil:write(postfix)
+            fil:write("\n\n\n")
+          end
+          
+          fil:close()
+        end
+      } end
+    )
+  end
 end
 
 local count = 0
@@ -113,7 +174,7 @@ for f in string.gmatch(flist, "[^\n]+") do
   table.insert(filz, f)
   count = count + 1
   
-  --if count == 10 then break end
+  if count == 10 then break end
 end
 
 for k, v in pairs(filz) do
--- compile_core.cpp
+++ compile_core.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
+#include <cstdio>
 #include <luabind/luabind.hpp>
 
 using namespace std;
 
+void poosh_char(lua_State *L, signed char val) {
+  if(val == -128) {
+    lua_pushnil(L);
+  } else {
+    lua_pushinteger(L, val);
+  }
+}
+
+void poosh_defloat(lua_State *L, signed int val) {
+  if(val == -2147483648) {
+    lua_pushnil(L);
+  } else {
+    lua_pushnumber(L, (float)val / 1000);
+  }
+}
+
 void slice_loc(lua_State *L, const std::string &dat) {
-  cout << dat << endl;
+  assert(dat.size() == 11);
+  signed char contid = dat[0];
+  signed int x = *(int*)&(dat[1]);  // go go gadget alignment error
+  signed int y = *(int*)&(dat[5]);
+  signed char rc = dat[9];
+  signed char rz = dat[10];
+  
+  poosh_char(L, contid);
+  poosh_defloat(L, x);
+  poosh_defloat(L, y);
+  poosh_char(L, rc);
+  poosh_char(L, rz);
 }
-void greet() {
-  std::cout << "hello world!\n";
-}
+
+class Image {
+  vector<unsigned int> data;
+  int wid;
+  int hei;
+public:
+  Image(int width, int height) {
+    assert(width >= 0);
+    assert(height >= 0);
+    wid = width;
+    hei = height;
+    data.resize(width * height, 0);
+  }
+  
+  void set(int x, int y, unsigned int pix) {
+    assert(x >= 0 && x < wid);
+    assert(y >= 0 && y < wid);
+    data[y * wid + x] = pix;
+  }
+};
 
 extern "C" int init(lua_State* L) {
   using namespace luabind;
@@ -18,7 +63,9 @@ extern "C" int init(lua_State* L) {
   module(L)
   [
     def("slice_loc", &slice_loc, raw(_1)),
-    def("greet", &greet)
+    class_<Image>("Image")
+      .def(constructor<int, int>())
+      .def("set", &Image::set)
   ];
 
   return 0;
