fcd1eb9a2e3b1c992ebedb98b5ea18aa5377dcf3
zorba <zorba-questhelper-git@pavlovian.net>
2009-01-09 20:11:46 -0800

    we are doing a thing

---
+++ chain.lua
@@ -0,0 +1,64 @@
+
+--[[
+
+Okay let's think about this.
+
+It takes the form of a series of chain blocks. Each chain block is a "manager" in some sort, in that it just represents the shape of the whole shebang. The actual implementation underlaying it doesn't really matter as much, so let's just build the framework first.
+
+Parameters to a block consist only of the construction function for the type it's meant to generate, and the blocks it's meant to pass things to.
+
+No need for inheritance, honestly.
+]]
+
+local ChainBlock = {}
+local ChainBlock_mt = { __index = ChainBlock }
+
+
+function ChainBlock_Create(factory, sortpred, linkfrom)
+  local ninst = {}
+  setmetatable(ninst, ChainItem_mt)
+  ninst.factory = factory
+  ninst.sortpred = sortpred
+  ninst.items = {}
+  ninst.data = {}
+  ninst.linkto = {}
+  ninst.process = function (key, subkey, value) for _, v in pairs(ninst.linkto) do v:Insert(key, subkey, value) end end
+  linkfrom.AddLinkTo(ninst)
+  return ninst
+end
+
+function ChainBlock:Insert(key, subkey, value)
+  if not subkey then
+    self:GetItem(key):Data(key, subkey, value, self.process)
+  else
+    table.insert(self:GetData(key), {subkey = subkey, value = value})
+  end
+end
+
+function ChainBlock:Finish()
+  for k, v in pairs(self.data) do
+    table.sort(v, function (a, b) return self.sortpred(a.subkey, b.subkey) end)
+    local item = self:GetItem(k)
+    for _, d in pairs(v) do
+      item:Data(k, d.subkey, d.value, self.process)
+    end
+  end
+  for _, v in pairs(self.items) do
+    v:Finish()
+  end
+  for _, v in pairs(self.linkto) do
+    v:Finish()
+  end
+  self.data = nil
+end
+
+function ChainBlock:AddLinkTo(item)
+  table.insert(linkto, item)
+end
+
+function ChainBlock:GetItem(key)
+  if not self.items[key] then
+    self.items[key] = self.factory()
+  end
+  return self.items[key]
+end
--- compile.lua
+++ compile.lua
@@ -1,2 +1,11 @@
-package.loadlib("/home/zorba/build/libcompile_core.so", "init")()
-greet()
+
+loadfile("chain.lua")()
+
+-- package.loadlib("/home/zorba/build/libcompile_core.so", "init")()
+-- greet()
+
+flist = io.popen("ls data/08"):read("*a")
+for f in string.gmatch(flist, "[^\n]+") do
+  print("data/08/" .. f)
+end
+
