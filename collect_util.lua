e611ebd73710ddb5be3bded00e9d463e5c1b005a
zorba <zorba-questhelper-git@pavlovian.net>
2009-01-18 00:47:50 -0800

    this will probably break things

--- collect_util.lua
+++ collect_util.lua
@@ -1,33 +1,33 @@
 QuestHelper_File["collect_util.lua"] = "Development Version"
 QuestHelper_Loadtime["collect_util.lua"] = GetTime()
 
-function IsMonsterGUID(guid)
+local function IsMonsterGUID(guid)
   QuestHelper: Assert(#guid == 18, "guid len " .. guid) -- 64 bits, plus the 0x prefix
   QuestHelper: Assert(guid:sub(1, 2) == "0x", "guid 0x-prefix " .. guid)
   return guid:sub(5, 5) == "3" or guid:sub(5, 5) == "5"
 end
 
-function GetMonsterUID(guid)
+local function GetMonsterUID(guid)
   QuestHelper: Assert(#guid == 18, "guid len " .. guid) -- 64 bits, plus the 0x prefix
   QuestHelper: Assert(guid:sub(1, 2) == "0x", "guid 0x-prefix " .. guid)
   QuestHelper: Assert(guid:sub(5, 5) == "3" or guid:sub(5, 5) == "5", "guid 3-prefix " .. guid)  -- It *shouldn't* be a player or a pet by the time we've gotten here. If so, something's gone wrong.
   return guid:sub(9, 18)  -- here's our actual identifier
 end
 
-function GetMonsterType(guid)
+local function GetMonsterType(guid)
   QuestHelper: Assert(#guid == 18, "guid len " .. guid) -- 64 bits, plus the 0x prefix
   QuestHelper: Assert(guid:sub(1, 2) == "0x", "guid 0x-prefix " .. guid)
   QuestHelper: Assert(guid:sub(5, 5) == "3" or guid:sub(5, 5) == "5", "guid 3-prefix " .. guid)  -- It *shouldn't* be a player or a pet by the time we've gotten here. If so, something's gone wrong.
   return tonumber(guid:sub(9, 12), 16)  -- here's our actual identifier
 end
 
-function GetItemType(link, vague)
+local function GetItemType(link, vague)
   return tonumber(string.match(link,
     (vague and "" or "^") .. "|cff%x%x%x%x%x%x|Hitem:(%d+):[%d:-]+|h%[[^%]]*%]|h|r".. (vague and "" or "$") 
   ))
 end
   
-function GetQuestType(link)
+local function GetQuestType(link)
   return tonumber(string.match(link,
     "^|cff%x%x%x%x%x%x|Hquest:(%d+):[%d-]+|h%[[^%]]*%]|h|r$"
   )), tonumber(string.match(link,
