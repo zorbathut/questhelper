8a309cde2a612347d8f67b9ca6252a66792385df
zorba <zorba-questhelper-git@pavlovian.net>
2009-02-18 22:09:56 -0800

    fix upgrading

--- collect_upgrade.lua
+++ collect_upgrade.lua
@@ -6,9 +6,7 @@ function QH_Collector_Upgrade(chunk)
   
   if chunk.version == 1 then
     -- We basically just want to clobber all our old route data, it's not worth storing - it's all good data, it's just that we don't want to preserve relics of the old location system.
-    for _, v in pairs(chunk) do
-      v.traveled = nil
-    end
+    chunk.traveled = nil
     
     chunk.version = 2
   end
@@ -16,21 +14,17 @@ function QH_Collector_Upgrade(chunk)
   if chunk.version == 2 then
     -- Originally I split the zones based on locale. Later I just split everything based on locale. Discarding old data rather than doing the gymnastics needed to preserve it.
     -- This is turning into a routine. :D
-    for _, v in pairs(chunk) do
-      v.zone = nil
-    end
+    chunk.zone = nil
     
     chunk.version = 3
   end
   
   if chunk.version == 3 then
     -- Screwed up the item collection code in instances. Obliterate old data, try again.
-    for locale, data in pairs(chunk) do
-      if data.item then
-        for id, dat in pairs(data.item) do
-          dat.equip_no = nil
-          dat.equip_yes = nil
-        end
+    if chunk.item then
+      for id, dat in pairs(chunk.item) do
+        dat.equip_no = nil
+        dat.equip_yes = nil
       end
     end
     
@@ -39,16 +33,14 @@ function QH_Collector_Upgrade(chunk)
   
   if chunk.version == 4 then
     -- Munged the shops rather badly. Whoopsydaisy.
-    for locale, data in pairs(chunk) do
-      if data.monster then
-        local nv = {}
-        for id, dat in pairs(data.monster) do
-          if type(dat) == "table" then
-            nv[id] = dat
-          end
+    if chunk.monster then
+      local nv = {}
+      for id, dat in pairs(chunk.monster) do
+        if type(dat) == "table" then
+          nv[id] = dat
         end
-        data.monster = nv
       end
+      chunk.monster = nv
     end
     
     chunk.version = 5
