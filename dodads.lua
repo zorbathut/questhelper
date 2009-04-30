6a05b072750e8d041c358d49a3d1cc015532a514
zorba <zorba-questhelper-git@pavlovian.net>
2009-04-30 05:07:59 -0700

    duh

--- dodads.lua
+++ dodads.lua
@@ -622,10 +622,10 @@ function QuestHelper:RemoveWaypointCallback(cb)
 end
 
 function QuestHelper:InvokeWaypointCallbacks(c, z, x, y, desc)
-  QuestHelper: Assert(type(c) == "number")
-  QuestHelper: Assert(type(z) == "number")
-  QuestHelper: Assert(type(x) == "number")
-  QuestHelper: Assert(type(y) == "number")
+  QuestHelper: Assert(not c or type(c) == "number")
+  QuestHelper: Assert(not z or type(z) == "number")
+  QuestHelper: Assert(not x or type(x) == "number")
+  QuestHelper: Assert(not y or type(y) == "number")
   if c ~= last_c or z ~= last_z or x ~= last_x or y ~= last_y or desc ~= last_desc then
     last_c, last_z, last_x, last_y, last_desc = c, z, x, y, desc
     for cb in pairs(callbacks) do
