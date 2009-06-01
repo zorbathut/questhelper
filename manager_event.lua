42e90e5b8b00f9cbde766aeca332d73d0026c500
zorba <zorba-questhelper-git@pavlovian.net>
2009-06-01 02:46:32 -0700

    instrumentation go

--- manager_event.lua
+++ manager_event.lua
@@ -6,20 +6,41 @@ QuestHelper_Loadtime["manager_event.lua"] = GetTime()
 
 -- File exists to centralize all the event hooks in one place. QH should never (rarely) eat CPU without going through this file. As a nice side effect, I can use this to measure when QH is using CPU. yaaaaay
 
+local next_started = false
+
+local time_used = 0
 
 local EventRegistrar = {}
 
+local OnUpdate_Keyed = {}
+
 local qh_event_frame = CreateFrame("Frame")
 
-local function OnEvent(_, event, ...)
-  local tstart = GetTime()
-  for _, v in pairs(EventRegistrar[event]) do
-    v.func(...)
+local function wraptime(ident, func, ...)
+  local st
+  if qh_loud_and_annoying then
+    st = GetTime()
   end
-  QH_Timeslice_Increment(GetTime() - tstart, "collect_event")
+  func(...)
+  if qh_loud_and_annoying and GetTime() - st > 0.0025 then
+    QuestHelper: TextOut(string.format("Took way too long, %4f, at %s", (GetTime() - st) * 1000, ident))
+  end
+end
+
+local function OnEvent(_, event, ...)
+  if not next_started then next_started, time_used = true, 0 end
+  
+  if EventRegistrar[event] then 
+    local tstart = GetTime()
+    for _, v in pairs(EventRegistrar[event]) do
+      wraptime(v.id, v.func, ...)
+    end
+    time_used = time_used + (GetTime() - tstart)
+  end
 end
 
 qh_event_frame:UnregisterAllEvents()
+qh_event_frame:RegisterAllEvents() -- I wonder what the performance penalty of this actually is
 qh_event_frame:SetScript("OnEvent", OnEvent)
 
 function QH_Event(event, func, identifier)
@@ -31,7 +52,7 @@ function QH_Event(event, func, identifier)
   else
     if not identifier then identifier = "(unknown event " .. event .. ")" end
     if not EventRegistrar[event] then
-      qh_event_frame:RegisterEvent(event)
+      --qh_event_frame:RegisterEvent(event)
       EventRegistrar[event] = {}
     end
     table.insert(EventRegistrar[event], {func = func, id = identifier})
@@ -41,16 +62,24 @@ end
 
 local OnUpdate = {}
 local OnUpdateHigh = {}
-local function OnUpdateTrigger()
-  for _, v in pairs(OnUpdateHigh) do
-    v.func()
+local function OnUpdateTrigger(_, ...)
+  if not next_started then next_started, time_used = true, 0 end
+  
+  do
+    local tstart = GetTime()
+    for _, v in pairs(OnUpdateHigh) do
+      wraptime(v.id, v.func, ...)
+    end
+    
+    for _, v in pairs(OnUpdate) do
+      if v.func then wraptime(v.id, v.func, ...) end
+    end
+    time_used = time_used + (GetTime() - tstart)
   end
   
-  for _, v in pairs(OnUpdate) do
-    v.func()
-  end
+  QH_Timeslice_Work(time_used)
   
-  QH_Timeslice_Work()
+  next_started = false
 end
 
 function QH_OnUpdate(func, identifier)
@@ -67,5 +96,10 @@ qh_event_frame:SetScript("OnUpdate", OnUpdateTrigger)
 
 
 function QH_Hook(target, hookname, func, identifier)
-  target:SetScript(hookname, func)
+  if hookname == "OnUpdate" then
+    OnUpdate[target] = {func = func and function (...) func(target, ...) end, id = identifier}
+  else
+    local ide = string.format("(unknown %s/%s)", tostring(target), hookname)
+    target:SetScript(hookname, function (...) wraptime(ide, func, ...) end)
+  end
 end
