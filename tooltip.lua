5c7181634bd77831fccc0eca4ddbaac07038fa01
zorba <zorba-questhelper-git@pavlovian.net>
2009-10-18 13:15:55 -0700

    update creature ids

--- tooltip.lua
+++ tooltip.lua
@@ -144,7 +144,11 @@ local function GetMonsterType(guid)
   QuestHelper: Assert(#guid == 18, "guid len " .. guid) -- 64 bits, plus the 0x prefix
   QuestHelper: Assert(guid:sub(1, 2) == "0x", "guid 0x-prefix " .. guid)
   QuestHelper: Assert(guid:sub(5, 5) == "3" or guid:sub(5, 5) == "5", "guid 3-prefix " .. guid)  -- It *shouldn't* be a player or a pet by the time we've gotten here. If so, something's gone wrong.
-  return tonumber(guid:sub(9, 12), 16)  -- here's our actual identifier
+  if GetBuildInfo():sub(1, 3) == "3.2" then
+    return tonumber(guid:sub(9, 12), 16)  -- here's our actual identifier
+  else
+    return tonumber(guid:sub(7, 10), 16)  -- 3.3 and in the future, including 0.3.0
+  end
 end
 
 local function GetItemType(link, vague)
